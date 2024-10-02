c8d1b58f678d18cb89ba84224112d5e382914b7b
Ayaz Ibrahimov <ayaz.ibrahimov.1998@mail.ru>
2024-10-02 19:08:57 +0400

    usage of useCallBack optimize funktion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useMemo, useState } from "react";
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 // import Test from "./Test"
 
@@ -28,9 +28,12 @@ function App() {
         )
       : posts;
 
-  function handleAddPost(post) {
+  
+
+  const handleAddPost = useCallback((post)=>{
     setPosts((posts) => [post, ...posts]);
-  }
+  },[])    
+
 
   function handleClearPosts() {
     setPosts([]);
@@ -69,7 +72,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive archiveOptios={archiveOptios} />
+      <Archive archiveOptios={archiveOptios} onAddPost={handleAddPost}  />
       <Footer />
     </section>
   );
@@ -171,7 +174,7 @@ function List({ posts }) {
 
 
 const Archive = memo(
-  function Archive({ archiveOptios }) {
+  function Archive({ archiveOptios,onAddPost }) {
     // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
     const [posts] = useState(() =>
       // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -194,7 +197,7 @@ const Archive = memo(
                 <p>
                   <strong>{post.title}:</strong> {post.body}
                 </p>
-                {/* <button onClick={() => onAddPost(post)}>Add as new post</button> */}
+                <button onClick={() => onAddPost(post)}>Add as new post</button>
               </li>
             ))}
           </ul>
